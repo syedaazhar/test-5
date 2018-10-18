2f13aaa91527aede77d999a3579c409e720690a0
syedaazhar <38943549+syedaazhar@users.noreply.github.com>
2018-10-18 17:46:01 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const options  = {
+    duration: 100
+};
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.slider');
+    var instances = M.Slider.init(elems, options);
+  });
+
